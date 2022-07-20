1da3f31bd099d4c8608488596ad97902232b46dc
Russell Ochoa <ochoa.russell@gmail.com>
2022-07-20 18:49:40 -0500

    full CRUD JSON API

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router()
+
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.get('/plants/:id', controllers.getPlantById)
+
+router.post('/plants', controllers.createPlant)
+
+router.put('/plants/:id', controllers.updatePlant)
+
+router.delete('/plants/:id', controllers.deletePlant)
+
+module.exports = router
+router.get('/plants', controllers.getAllPlants)
